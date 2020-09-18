0f198e9bcfaf3060fe8a399cce75adb70e119249
ElmerAvila09 <62662713+ElmerAvila09@users.noreply.github.com>
2020-09-18 11:26:28 -0500

    Documentacion General
    Documentacion del juego en general para comprenderlo

--- cannon.py
+++ cannon.py
@@ -1,39 +1,69 @@
+"""
+Intituto Tecnologico de Estudios Superiores de Monterrey
+Equipo "Default":
+Daniel de Zamacona Madero - A01570576
+Elmer Osiel Avila Vargas - A00826359
+El programa despliega un juego con objetivos a destruir a partir de proyectiles que aparecen al hacer click en la pantalla.
+Fecha de Modificacion: 17/9/2020
+"""
 from random import randrange
 from turtle import *
 from freegames import vector
 
+# Declaracion de variables iniciales
 ball = vector(-200, -200)
 speed = vector(0, 0)
 targets = []
 
+'''
+Responde al click en la pantalla de juego
+Entrada: Posicion vectorial de donde se hace click
+Salida: Ninguna
+'''
 def tap(x, y):
-    "Respond to screen tap."
+
     if not inside(ball):
         ball.x = -199
         ball.y = -199
         speed.x = (x + 200) / 25
         speed.y = (y + 200) / 25
 
+'''
+Revisar si una poscicion esta dentro de la pantalla
+Entrada: Vector a revisar
+Salida: Booleano, true si la posicion se encuntra dentro de los limites.
+'''
 def inside(xy):
-    "Return True if xy within screen."
+    # Return con la comparacion de los limites de la pantalla
     return -200 < xy.x < 200 and -200 < xy.y < 200
 
+'''
+Dibuja las balas y los objetivos
+Entrada: Ninguna
+Salida: Ninguna
+'''
 def draw():
-    "Draw ball and targets."
     clear()
 
+    # Dibuja el punto que simulara el objetivo, color azul
     for target in targets:
         goto(target.x, target.y)
         dot(20, 'blue')
 
+    # Dibuja el punto que simulara el proyectil, color rojo
     if inside(ball):
         goto(ball.x, ball.y)
         dot(6, 'red')
 
     update()
 
+'''
+Genera todo el movimiento dentro del juego
+Entrada: Ninguna
+Salida: Ninguna
+'''
 def move():
-    "Move ball and targets."
+    # En cada actualizacion de pantalla se hace un random, si este es 0 entonces crea objetivo
     if randrange(40) == 0:
         y = randrange(-150, 150)
         target = vector(200, y)
@@ -42,6 +72,7 @@ def move():
     for target in targets:
         target.x -= 0.5
 
+    # Se da velocidad negativa en y para que el proyectil se caiga, es decir, simule una ruta parabolica
     if inside(ball):
         speed.y -= 0.35
         ball.move(speed)
@@ -55,10 +86,12 @@ def move():
 
     draw()
 
+    # Revisa si un objetivo no esta dentro de los rangos, en ese caso termina el movimiento
     for target in targets:
         if not inside(target):
             return
 
+    # Actualizaciones cada 50 milesimas de segundo de segundo
     ontimer(move, 50)
 
 setup(420, 420, 370, 0)
